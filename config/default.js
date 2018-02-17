d0821c1817f27409b22e210bb0a551a1efbc514a
Lumiboy <tha_lumi@hotmail.com>
2018-02-17 01:17:33 +0100

    btcz to zel

--- config/default.js
+++ config/default.js
@@ -3,14 +3,10 @@ module.exports = {
     port: 3434,
   },
   apiServers: [
-    'https://explorer.btcz.rocks',
-    'https://bitcoinz.ph',
-    'http://54.174.68.212:3001',
-    'http://212.48.90.191:3001',
+    'https://explorer2.zel.cash',
+    'http://explorer.zel.cash',
   ],
   bwsServers: [
-    'https://bws.btcz.rocks',
-    'https://bws.bitcoinz.ph',
-    'http://212.48.90.191:3232',
+    'https://192.168.128.10:3232',
   ],
 };
